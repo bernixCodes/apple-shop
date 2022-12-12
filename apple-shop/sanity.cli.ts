23dfd1cc3367087842eb0192b4a6c122a2e5a490
bernice <bernice@hubtel.com>
2022-12-12 21:42:42 +0000

    sanity setup

---
+++ apple-shop/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'jppi8g9y',
+    dataset: 'production'
+  }
+})
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="text-red-800 bg-red-700">Heyy</h1>
+      <h1 className="text-white bg-red-700">Heyy</h1>
     </div>
   );
 }
