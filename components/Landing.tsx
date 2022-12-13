66be0d41a73a45a7065c2111b21f2fcc2ad35eed
bernice <bernice@hubtel.com>
2022-12-13 18:43:47 +0000

    new promos

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Button from "./Button";
 
 function Landing() {
   return (
     <section className="sticky top-0 mx-auto flex h-screen max-w-[1350px] items-center justify-between px-8">
-      <div className="space-y-8">
+      <div className="space-y-8 ">
         <h1 className="space-y-3 text-5xl font-semibold tracking-wide lg:text-6xl xl:text-7xl">
           <span className="block bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent">
             Powered
@@ -12,8 +13,8 @@ function Landing() {
           <span className="block">Driven By Values</span>
         </h1>
 
-        <div>
-          {/* <Button titl='Buy Now'/> */}
+        <div className="space-x-8">
+          <Button title="Buy Now" />
           <a className="relative cursor-pointer text-lg font-medium before:absolute before:inset-x-0 before:-bottom-1.5 before:h-0.5 before:origin-left before:scale-x-0 before: transform before:rounded-bl before:bg-black before:transition-all before:duration-200 hover:before:scale-x-100">
             Learn More
           </a>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
 import Landing from "./../components/Landing";
+import { Tab } from "@headlessui/react";
+import { GetServerSideProps } from "next";
 
 export default function Home() {
   return (
@@ -14,9 +16,50 @@ export default function Home() {
       </Head>
 
       <Header />
-      <main>
+      <main className="relative h-[200vh] bg-[#e7ecee]">
         <Landing />
       </main>
+      <section className="relative z-40 -mt-[100vh] min-h-screen bg-[#1b1b1b]">
+        <div className="space-y-10 pt-16">
+          <h1 className="text-center text-4xl md:text-5xl font-medium tracking-wide text-white">
+            New Promos
+          </h1>
+          <Tab.Group>
+            <Tab.List className="flex justify-center">
+              {/* {categories.map((category) => (
+                <Tab
+                  key={category._id}
+                  id={category._id}
+                  className={({ selected }) =>
+                    `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
+                      selected
+                        ? "borderGradient bg-[#35383C] text-white"
+                        : "border-b-2 border-[#35383C] text-[#747474]"
+                    }`
+                  }
+                >
+                  {category.title}
+                </Tab>
+              ))} */}
+            </Tab.List>
+            <Tab.Panels className="mx-auto max-w-fit pt-10 pb-24 sm:px-4">
+              {/* <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(1)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(2)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel> */}
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+      </section>
     </div>
   );
 }
+
+//backend code
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  // const categories = await fetchCategories()
+  return {
+    props: {},
+  };
+};
